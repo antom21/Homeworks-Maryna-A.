e15b37b4ce3a92fff4916bba6f42e9b480f391b4
antom21 <antonova.maryna@yandex.ua>
2016-11-01 17:28:23 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignemnt_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x, number, fact;
+            Console.WriteLine("Enter the Number");
+            number = int.Parse(Console.ReadLine());
+            fact = number;
+            for (x = number - 1; x >= 1; x--)
+            {
+                fact = fact * x;
+            }
+            Console.WriteLine("\nFactorial of Given Number is: " + fact);
+            Console.ReadLine();
+
+        }
+    }
+}
