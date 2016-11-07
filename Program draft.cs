25a004c28338a755080cd957ccec73d8b699b4e8
antom21 <antonova.maryna@yandex.ua>
2016-11-07 00:41:06 -0500

    Add files via upload

---
+++ Program draft.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_draft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+
+
+
+public class Program
+
+        {
+
+            public static void Main()
+
+            {
+
+                {
+
+                    int num, n;
+
+
+
+
+
+                    Console.WriteLine("Enter a number of factorials");
+
+                    num = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+                    n = num; // assign n to num
+
+
+
+                    while (num > 0)
+
+                    {
+
+                        for (int i = n - 1; i > 0; i--)
+
+                        {
+
+                            n = n * i;
+
+                        }
+
+                        Console.WriteLine("Factorial of {0}! = {1}\n", num, n);
+
+                        num--;
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+    }
+}
